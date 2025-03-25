c79a19756dde18b2c801dfa21fb321e4b0371201
ModsOfPantheon <194365524+ModsOfPantheon@users.noreply.github.com>
2025-03-25 20:37:59 +0000

    Initial commit

---
+++ LockTooltipPositions/ModMain.cs
@@ -0,0 +1,8 @@
+using MelonLoader;
+
+namespace LockTooltipPositions;
+
+public class ModMain : MelonMod
+{
+    public const string ModVersion = "1.0.0";
+}
---
+++ LockTooltipPositions/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using LockTooltipPositions;
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(ModMain), nameof(LockTooltipPositions), ModMain.ModVersion, "Roast")]
+[assembly: MelonGame("Visionary Realms", "Pantheon")]
