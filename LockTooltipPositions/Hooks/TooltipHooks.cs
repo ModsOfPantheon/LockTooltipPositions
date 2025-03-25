c6e2edb9537d2fb2e59264c8036f2b85033818dc
ModsOfPantheon <194365524+ModsOfPantheon@users.noreply.github.com>
2025-03-25 21:55:32 +0000

    Update assemblyinfo

--- LockTooltipPositions/Hooks/TooltipHooks.cs
+++ LockTooltipPositions/Hooks/TooltipHooks.cs
@@ -1,6 +1,5 @@
 using HarmonyLib;
 using Il2Cpp;
-using UnityEngine;
 
 namespace LockTooltipPositions.Hooks;
 
--- LockTooltipPositions/Properties/AssemblyInfo.cs
+++ LockTooltipPositions/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 using LockTooltipPositions;
 using MelonLoader;
 
-[assembly: MelonInfo(typeof(ModMain), nameof(LockTooltipPositions), ModMain.ModVersion, "Roast")]
+[assembly: MelonInfo(typeof(ModMain), nameof(LockTooltipPositions), ModMain.ModVersion, "ModsOfPantheon")]
 [assembly: MelonGame("Visionary Realms", "Pantheon")]
